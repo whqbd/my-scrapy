bb2d11aa762ad0902ea1aad9069a6307bd205c1f
whqbd <whqbd@126.com>
2018-06-08 17:59:09 +0800

    这是个测试项目

---
+++ com/scrapy/whq/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time : 2018/6/6 15:19
+# @Author : whq
+# @File : __init__.py.py
---
+++ com/scrapy/whq/my_scrapy.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time : 2018/6/6 15:21
+# @Author : whq
+# @File : my_scrapy.py
+
+import scrapy
